0956ba37892f4d8317b9e162ce9b142b4c336d3c
Julio César <44672390+czjotadev@users.noreply.github.com>
2024-03-14 10:58:25 -0300

    Update update-user.dto.ts

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,18 +1,23 @@
-import { IsBoolean, IsString } from 'class-validator';
+import { IsBoolean, IsString, IsOptional } from 'class-validator';
 
 export class UpdateUserDto {
+  @IsOptional()
   @IsString()
   name?: string;
 
+  @IsOptional()
   @IsString()
   address?: string;
 
+  @IsOptional()
   @IsString()
   phone?: string;
 
+  @IsOptional()
   @IsBoolean()
   admin?: boolean;
 
+  @IsOptional()
   @IsString()
   password?: string;
 }
