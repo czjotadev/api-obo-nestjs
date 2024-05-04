5c61a49e0ee5b931717078c2931894ad3fb7d1b1
Julio Cesar <juliocesar_mcz@hotmail.com>
2024-05-04 17:28:22 -0300

    removendo instagram dos produtos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -3,7 +3,6 @@ export class CreateProductDto {
   name: string;
   urlName?: string;
   description: string;
-  instagram: string;
   productCategoryId: string;
   active: string;
   showcase: string;
